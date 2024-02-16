df9f84f5991069eca3ebc0e169526d90931c79b1
rahmannudn <rahmannudn0@gmail.com>
2024-02-16 22:46:22 +0800

    menambahkan fitur back to menu

--- src/lesson_4_group/main.py
+++ src/lesson_4_group/main.py
@@ -1,46 +1,61 @@
-uangMasuk = [200,500,1000]
-uangKeluar = [40,100]
+uangMasuk = [200, 500, 1000]
+uangKeluar = [40, 100]
 jumlahUangMasuk = 0
 jumlahUangKeluar = 0
 
+
 def calcUangMasuk():
     for uang in uangMasuk:
-        jumlahUangMasuk+=uang
+        global jumlahUangMasuk
+        jumlahUangMasuk += uang
+
 
 def calcUangKeluar():
     for uang in uangKeluar:
-        jumlahUangKeluar+=uang
+        global jumlahUangKeluar
+        jumlahUangKeluar += uang
 
-def listMenu():
-    try:
-        print('SILAHKAN PILIH MENU')
-        print('0 : Masuk')
-        print('1 : Keluar')
-        print('2 : Sum Masuk')
-        print('3 : Sum Keluar')
-        print('4 : Sum ALL')
-        selectedMenu = int(input('Pilih Menu: '))
 
-        if(selectedMenu == 0):
-            print('List Uang Masuk : ')
-            for uang in uangMasuk:
-                print(f"- {uang}")
-        elif(selectedMenu == 1):
-            print('List Uang keluar : ')
-            for uang in uangKeluar:
-                print(f'- {uang}')
-        elif(selectedMenu == 2):
-            print(f'Jumlah Uang Masuk : {jumlahUangMasuk}')
-        elif(selectedMenu == 3):
-            print(f'Jumlah Uang Keluar : {jumlahUangKeluar}')
-        elif(selectedMenu == 4):
-            print(f'Jumlah Sisa Uang (uang masuk - keluar) : {jumlahUangMasuk - jumlahUangKeluar}')
-    except:
-        print('tolong pilih menu dengan benar!')
+def operations(menu):
+    if menu == 0:
+        print("List Uang Masuk : ")
+        for uang in uangMasuk:
+            print(f"- {uang}")
+    elif menu == 1:
+        print("List Uang keluar : ")
+        for uang in uangKeluar:
+            print(f"- {uang}")
+    elif menu == 2:
+        print(f"Jumlah Uang Masuk : {jumlahUangMasuk}")
+    elif menu == 3:
+        print(f"Jumlah Uang Keluar : {jumlahUangKeluar}")
+    elif menu == 4:
+        print(
+            f"Jumlah Sisa Uang (uang masuk - keluar) : {jumlahUangMasuk - jumlahUangKeluar}"
+        )
+    else:
+        print("pilih menu dengan benar!!!")
+
+    print('Tekan 0 Untuk kembali ke menu')
+    backToMenu = int(input(" "))
+    if backToMenu == 0:
+        showMenu()
+
+
+def showMenu():
+        print("SILAHKAN PILIH MENU")
+        print("0 : Masuk")
+        print("1 : Keluar")
+        print("2 : Sum Masuk")
+        print("3 : Sum Keluar")
+        print("4 : Sum ALL")
+        selectedMenu = int(input("Pilih Menu: "))
+        operations(selectedMenu)
 
 def init():
     calcUangMasuk()
     calcUangKeluar()
-    menu = listMenu()
+    showMenu()
+
 
 init()
